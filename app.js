4cf77510952ebac933422711a901a5671062f8f0
mohit1900 <108678399+mohit1900@users.noreply.github.com>
2023-03-10 11:05:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: '*',
+    origin: 'https://vhandleuss.netlify.app/',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
