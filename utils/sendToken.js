9cd439df6a09926715453678d717cae42b26fc20
mohit1900 <108678399+mohit1900@users.noreply.github.com>
2023-03-11 00:57:04 +0530

    Update sendToken.js

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,7 +6,7 @@ export const sendToken = (res, user, message, statusCode = 200) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
-    domain: 'https://vhandleuss.netlify.app'
+    domain: '.netlify.app'
   };
 
   res.status(statusCode).cookie("token", token, options).json({
