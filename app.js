166f4f28044d21ecfe864703507404c6fec9de1b
mohit1900 <108678399+mohit1900@users.noreply.github.com>
2023-03-10 23:25:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,13 +27,7 @@ app.use(
   })
 );
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  next();
-});
+
 
 import user from "./routes/userRoutes.js";
 import admin from "./routes/adminRoutes.js";
