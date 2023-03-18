6948ad4ad944bf022c8c87cba624f652a874d1b5
mohit1900 <mohitchandra723@gmail.com>
2023-03-18 19:09:10 +0530

    fixed origin

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://vhandleuss.netlify.app",
+    origin: [
+      "https://vhandleuss.netlify.app",
+      "https://ussfrontendtest.vercel.app/",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
